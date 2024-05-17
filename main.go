84637b7684fb6a1bde0fe624f6247ac08a4d8b18
Made Indra <made.indra@protonmail.com>
2024-05-17 09:15:29 +0700

    print usage

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
+func main() {
+	var help bool
+	var inputPath, outputPath string
+
+	flag.BoolVar(&help, "h", false, "Tampilkan cara menggunakan")
+	flag.BoolVar(&help, "help", false, "Tampilkan cara menggunakan")
+
+	flag.StringVar(&inputPath, "i", "", "Lokasi file JSON sebagai input")
+	flag.StringVar(&inputPath, "input", "", "Lokasi file JSON sebagai input")
+	flag.StringVar(&outputPath, "o", "", "Lokasi file JSON sebagai output")
+	flag.StringVar(&outputPath, "output", "", "Lokasi file JSON sebagai output")
+
+	flag.Parse()
+
+	if help || inputPath == "" || outputPath == "" {
+		printUsage()
+		os.Exit(0)
+	}
+}
+
+func printUsage() {
+	fmt.Println("Usage: mockdata [-i | --input] <input file> [-o | --output] <output file>")
+	fmt.Println("-i --input: File input berupa JSON sebagai template")
+	fmt.Println("-o --output: File output berupa JSON sebagai hasil")
 }

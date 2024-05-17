48473028379134b765f29cd9001ca574ab3b0f4c
Made Indra <made.indra@protonmail.com>
2024-05-17 10:21:24 +0700

    completed generator functions

---
+++ data/address.go
@@ -0,0 +1,6 @@
+package data
+
+var address = map[string][]string{
+	SUBTYPE_STREET: {"Tjut Meutia", "Mohammad Hoesni Thamrin", "Dr. K.H. Idham Chalid", "Frans Kaisiepo", "Dr G.S.S.J. Ratulangi", "Ir. H. Djuanda Kartawidjaja", "Dr. (H.C.) Ir. Soekarno", "Dr. (H.C.) Mohammad Hatta"},
+	SUBTYPE_CITY:   {"Jakarta", "Bandung", "Surabaya", "Medan", "Semarang", "Makassar", "Palembang", "Depok", "Bekasi", "Tangerang"},
+}
